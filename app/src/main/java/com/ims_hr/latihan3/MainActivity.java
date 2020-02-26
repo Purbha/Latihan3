9ef158cfba5402b2f996c29bc5dd1ed39f727a49
Purbha <purbha_6string@yahoo.co.id>
2020-02-26 20:31:44 +0700

    COMMIT PERTAMA

---
+++ app/src/main/java/com/ims_hr/latihan3/MainActivity.java
@@ -0,0 +1,35 @@
+package com.ims_hr.latihan3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button B_Submit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Inisial();
+        Listen_B_Submit();
+    }
+
+    private void Inisial() {
+        B_Submit = (Button) findViewById(R.id.button_Main_Submit);
+    }
+
+    private void Listen_B_Submit() {
+        B_Submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Ini adalah Pesan.", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+}
